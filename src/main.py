6bcf4dae80323ebc530e1e3b6cd6487f8fd665ee
Marc Dupuis <marcdupuis@Marcs-MacBook-Pro.local>
2014-05-05 22:09:14 -0400

    exploring the data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
 #!/bin/python
+
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+header = ['age', 'workclass', 'fnlwgt', 'education', 'unknown', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capita_loss', 'hours-per-week', 'native_country', 'income']
+data = pd.read_csv('../data/adult_data.csv', index_col = False, header = None, names = header)
+data = data.ix[0:32560,:]
+
+print data
+
+print np.mean(data.age)
+plt.hist(data.age, bins = 37)
+plt.show()
